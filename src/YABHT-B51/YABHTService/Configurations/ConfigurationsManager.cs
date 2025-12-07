3ccb5b8625f9b0ea6f82f44e0eeed09177c267f9
damien-r <11413651+damien-r@users.noreply.github.com>
2025-12-07 13:18:16 +0100

    Begin configuration reading

---
+++ src/YABHT-B51/YABHTService/Configurations/ConfigurationsManager.cs
@@ -0,0 +1,25 @@
+﻿using log4net;
+using YABHTService.Configurations.Models;
+
+namespace YABHTService.Configurations
+{
+    internal class ConfigurationsManager
+    {
+
+		private static readonly ILog _logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		private readonly IConfiguration _configuration;
+
+		public IList<RepositoryConfiguration> GetRepositories()
+		{
+			var configurationsFolder = _configuration.GetSection("YABHT").GetSection("General").GetValue("ConfigurationFolder", "Configs");
+
+			if (!Directory.Exists(configurationsFolder))
+			{
+				_logger.Warn($"Configurations folder '{configurationsFolder}' does not exist.");
+				return new List<RepositoryConfiguration>();
+			}
+
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/YABHT-B51/YABHTService/Configurations/Models/RepositoryConfiguration.cs
@@ -0,0 +1,6 @@
+﻿namespace YABHTService.Configurations.Models
+{
+    public record RepositoryConfiguration
+    {
+    }
+}
