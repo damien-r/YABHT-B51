58701f89c50038a28713cd0cf8d5c24765aa0971
damien-r <11413651+damien-r@users.noreply.github.com>
2025-12-07 13:08:18 +0100

    Make running rate configurable

--- src/YABHT-B51/YABHTService/YABHTWorker.cs
+++ src/YABHT-B51/YABHTService/YABHTWorker.cs
@@ -2,16 +2,26 @@ namespace YABHTService
 {
 
 
-    public class YABHTWorker() : BackgroundService
+    public class YABHTWorker : BackgroundService
     {
-        private static readonly log4net.ILog _logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		private readonly IConfiguration _configuration;
+
+		public YABHTWorker(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		private static readonly log4net.ILog _logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.Debug("Worker running");
-                await Task.Delay(1000, stoppingToken);
-            }
+
+                var runningRate = _configuration.GetSection("YABHT").GetSection("General").GetValue("RunningRate", TimeSpan.FromMinutes(10));
+                _logger.Debug($"Job done. Waiting for {runningRate}");
+                await Task.Delay(runningRate, stoppingToken);
+			}
         }
     }
 }
