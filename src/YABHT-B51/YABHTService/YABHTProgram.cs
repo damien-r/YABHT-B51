f2792eef554ea6796a41c0256a4456ec031a790b
damien-r <11413651+damien-r@users.noreply.github.com>
2025-12-07 15:03:00 +0100

    Set program as a service
    To avoid killed start
    Source: https://learn.microsoft.com/en-us/dotnet/core/extensions/windows-service

--- src/YABHT-B51/YABHTService/YABHTProgram.cs
+++ src/YABHT-B51/YABHTService/YABHTProgram.cs
@@ -5,6 +5,11 @@ using YABHTService.Repositories;
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "YABHT-B51";
+});
+
 builder.Services.AddHostedService<YABHTWorker>();
 builder.Services.AddSingleton<ConfigurationsManager>();
 builder.Services.AddSingleton<RepositoriesManager>();
