766774aacdc40931b955cf4c30ced8425ace646f
damien-r <11413651+damien-r@users.noreply.github.com>
2025-12-07 14:18:06 +0100

    Commit modified files

--- src/YABHT-B51/YABHTService/Repositories/RepositoriesManager.cs
+++ src/YABHT-B51/YABHTService/Repositories/RepositoriesManager.cs
@@ -1,11 +1,27 @@
-﻿using YABHTService.Configurations.Models;
+﻿using LibGit2Sharp;
+using log4net;
+using YABHTService.Configurations.Models;
 
 namespace YABHTService.Repositories;
 
 internal class RepositoriesManager
 {
+		private static readonly ILog _logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
     public void BackupRepository(RepositoryConfiguration repositoryConfiguration)
     {
-        throw new NotImplementedException();
+        _logger.Debug($"Analyze repository '{repositoryConfiguration.Name}'");
+        using var repository = new Repository(repositoryConfiguration.RepositoryPath);
+
+        Commands.Stage(repository, "*");
+        
+        var status = repository.RetrieveStatus();
+        int fileCount = status
+            .Count(s => s.State != FileStatus.Ignored &&
+                        s.State != FileStatus.Unaltered);
+        
+        if(fileCount > 0) _logger.Info($"Backup repository '{repositoryConfiguration.Name}' with {fileCount} files.");
+        var now = DateTimeOffset.Now;
+        var author = new Signature("YABHT", "", now);
+        var commit = repository.Commit($"[YABHT] Automated backup at {now} ", author, author);
     }
 }
