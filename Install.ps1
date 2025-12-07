58c9e9ff247e21f2a1fb0fba8185e969ff3699c9
damien-r <11413651+damien-r@users.noreply.github.com>
2025-12-07 15:24:22 +0100

    Stop and start service when installing it

--- Install.ps1
+++ Install.ps1
@@ -1,9 +1,15 @@
+# May require to run as administrator
+
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 Push-Location "$dir"
 
 cd .\src\YABHT-B51\YABHTService
-# May require to run as administrator
+
+sc.exe stop YABHT-B51
+
 dotnet publish -c Release --runtime win-x64 --framework net10.0 -o "C:\Program Files\B51\YABHT-B51\"
+
 sc.exe create YABHT-B51 binPath= "C:\Program Files\B51\YABHT-B51\YABHTService.exe" start= auto
 sc.exe description YABHT-B51 https://github.com/damien-r/YABHT-B51
+sc.exe start YABHT-B51
