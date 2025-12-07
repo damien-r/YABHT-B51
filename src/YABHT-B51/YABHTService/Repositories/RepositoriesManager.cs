540ee3a63918d363eae56610cbff373bd498df63
damien-r <11413651+damien-r@users.noreply.github.com>
2025-12-07 15:34:17 +0100

    Avoid false error
    `LibGit2Sharp.EmptyCommitException: No changes; nothing to commit.`

--- src/YABHT-B51/YABHTService/Repositories/RepositoriesManager.cs
+++ src/YABHT-B51/YABHTService/Repositories/RepositoriesManager.cs
@@ -6,16 +6,17 @@ namespace YABHTService.Repositories;
 
 internal class RepositoriesManager
 {
-		private static readonly ILog _logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-		private readonly IConfiguration _configuration;
+    private static readonly ILog _logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-        public RepositoriesManager(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
+    private readonly IConfiguration _configuration;
+
+    public RepositoriesManager(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
 
 
-        public void BackupRepository(RepositoryConfiguration repositoryConfiguration)
+    public void BackupRepository(RepositoryConfiguration repositoryConfiguration)
     {
         if (_configuration.GetValue<bool>("YABHT:General:AllowUnsafeRepositories"))
         {
@@ -23,18 +24,22 @@ internal class RepositoriesManager
             _logger.Debug("Set safe");
             GlobalSettings.SetOwnerValidation(false);
         }
-        
+
         _logger.Debug($"Analyze repository '{repositoryConfiguration.Name}'");
         using var repository = new Repository(repositoryConfiguration.RepositoryPath);
-        
+
         Commands.Stage(repository, "*");
-        
+
         var status = repository.RetrieveStatus();
         int fileCount = status.Count(s => s.State != FileStatus.Ignored && s.State != FileStatus.Unaltered);
-        
-        if(fileCount > 0) _logger.Info($"Backup repository '{repositoryConfiguration.Name}' with {fileCount} files.");
-        var now = DateTimeOffset.Now;
-        var author = new Signature("YABHT-B51", "YABHT-B51@no-reply.com", now);
-        var commit = repository.Commit($"[YABHT] Automated backup at {now} ", author, author);
+
+        if (fileCount > 0)
+        {
+            _logger.Info($"Backup repository '{repositoryConfiguration.Name}' with {fileCount} files.");
+            var now = DateTimeOffset.Now;
+            var author = new Signature("YABHT-B51", "YABHT-B51@no-reply.com", now);
+            var commit = repository.Commit($"[YABHT] Automated backup at {now} ", author, author);
+        }
     }
+
 }
